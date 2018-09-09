02c9ea7196ed20def0b675602bc5510fe2852bad
Crislei Terassi Sorrilha <crts.ms@gmail.com>
2018-09-09 12:37:10 -0700

    add loading in async operations screens

--- WebApplication/App_Start/BundleConfig.cs
+++ WebApplication/App_Start/BundleConfig.cs
@@ -27,7 +27,8 @@ namespace WebApplication
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/Site.css",
+                      "~/Content/loading.css"));
         }
     }
 }
