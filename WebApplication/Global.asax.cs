6eb6692ad1a33b87f82958c8f0cc40f3b649d7b3
Crislei Terassi Sorrilha <crts.ms@gmail.com>
2019-06-19 19:50:43 -0300

    Fixing error processing in global app

--- WebApplication/Global.asax.cs
+++ WebApplication/Global.asax.cs
@@ -23,35 +23,35 @@ namespace WebApplication
 
         protected void Application_Error(object sender, EventArgs e)
         {
-            //var app = (MvcApplication)sender;
-            //var context = app.Context;
-            //var ex = app.Server.GetLastError();
-            //context.Response.Clear();
-            //context.ClearError();
+            var app = (MvcApplication)sender;
+            var context = app.Context;
+            var ex = app.Server.GetLastError();
+            context.Response.Clear();
+            context.ClearError();
 
-            //var httpException = ex as HttpException;
+            var httpException = ex as HttpException;
 
-            //var routeData = new RouteData();
-            //routeData.Values["controller"] = "Error";
-            //routeData.Values["exception"] = ex;
-            //routeData.Values["action"] = "Http500";
+            var routeData = new RouteData();
+            routeData.Values["controller"] = "Error";
+            routeData.Values["exception"] = ex;
+            routeData.Values["action"] = "Http500";
 
-            //if (httpException != null)
-            //{
-            //    switch (httpException.GetHttpCode())
-            //    {
-            //        case 404:
-            //            routeData.Values["action"] = "Http404";
-            //            break;
-            //        case 500:
-            //            routeData.Values["action"] = "Http500";
-            //            break;
-            //    }
-            //}
+            if (httpException != null)
+            {
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        routeData.Values["action"] = "Http404";
+                        break;
+                    case 500:
+                        routeData.Values["action"] = "Http500";
+                        break;
+                }
+            }
 
-            //Response.TrySkipIisCustomErrors = true;
-            //IController controller = new ErrorController();
-            //controller.Execute(new RequestContext(new HttpContextWrapper(context), routeData));
+            Response.TrySkipIisCustomErrors = true;
+            IController controller = new ErrorController();
+            controller.Execute(new RequestContext(new HttpContextWrapper(context), routeData));
 
         }
     }
